b75b1f28c5a66c0f583eab6792f2e034a8b9d713
Michael Paquette <jotha885@gmail.com>
2019-07-04 16:08:12 +0200

    added return value

--- guess_tau_from_b.py
+++ guess_tau_from_b.py
@@ -4,7 +4,7 @@ def main(target_b, Gmax):
     magic_number = 702129.6736002087
     tau = ((float(target_b)*1e9) / (float(Gmax)**2*magic_number))**(1/3.)
     print(tau)
-
+    return tau
 
 if __name__ == "__main__":
     import sys
